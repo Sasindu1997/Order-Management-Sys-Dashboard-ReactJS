34e2672bb01b03ebbe740ed94a03aeb13aab2199
Sasindu1997 <sasinduusgalhewa@gmail.com>
2023-06-05 03:12:39 +0530

    API functions

--- src/layouts/store/index.jsx
+++ src/layouts/store/index.jsx
@@ -39,109 +39,6 @@ function Store() {
     const { columns, rows } = authorsTableData();
     const { columns: pColumns, rows: pRows } = projectsTableData();
 
-<<<<<<< HEAD
-    const handleClick = () => {
-
-    }
-
-    return ( <
-        DashboardLayout >
-        <
-        DashboardNavbar / >
-        <
-        MDBox pt = { 6 }
-        pb = { 3 } >
-        <
-        Grid container spacing = { 3 } >
-        <
-        Grid item xs = { 12 }
-        md = { 6 }
-        lg = { 3 } >
-        <
-        MDBox mb = { 1.5 } >
-        <
-        ComplexStatisticsCard color = "dark"
-        icon = "weekend"
-        title = "Bookings"
-        count = { 281 }
-        percentage = {
-            {
-                color: "success",
-                amount: "+55%",
-                label: "than lask week",
-            }
-        }
-        onClickCard = {
-            () => handleClick }
-        /> <
-        /MDBox> <
-        /Grid> <
-        Grid item xs = { 12 }
-        md = { 6 }
-        lg = { 3 } >
-        <
-        MDBox mb = { 1.5 } >
-        <
-        ComplexStatisticsCard icon = "leaderboard"
-        title = "Today's Users"
-        count = "2,300"
-        percentage = {
-            {
-                color: "success",
-                amount: "+3%",
-                label: "than last month",
-            }
-        }
-        /> <
-        /MDBox> <
-        /Grid> <
-        Grid item xs = { 12 }
-        md = { 6 }
-        lg = { 3 } >
-        <
-        MDBox mb = { 1.5 } >
-        <
-        ComplexStatisticsCard color = "success"
-        icon = "store"
-        title = "Revenue"
-        count = "34k"
-        percentage = {
-            {
-                color: "success",
-                amount: "+1%",
-                label: "than yesterday",
-            }
-        }
-        /> <
-        /MDBox> <
-        /Grid> <
-        Grid item xs = { 12 }
-        md = { 6 }
-        lg = { 3 } >
-        <
-        MDBox mb = { 1.5 } >
-        <
-        ComplexStatisticsCard color = "primary"
-        icon = "person_add"
-        title = "Followers"
-        count = "+91"
-        percentage = {
-            {
-                color: "success",
-                amount: "",
-                label: "Just updated",
-            }
-        }
-        /> <
-        /MDBox> <
-        /Grid> <
-        /Grid> <
-        /MDBox> <
-        Footer / >
-        <
-        /DashboardLayout>
-    );
-=======
   const handleClick = () => {
 
   }
@@ -223,7 +120,6 @@ function Store() {
       <Footer />
     </DashboardLayout>
   );
->>>>>>> cd55bc99ddd01b02ca64cac2746ba96a42c5f5ff
 }
 
 export default Store;
